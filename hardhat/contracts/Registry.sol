b1ae198599a3d556e6a37af4fb4f37ef5a7c9638
nick-dev123 <144809451+nick-dev123@users.noreply.github.com>
2024-12-15 11:50:44 +0200

    feat: main functions

--- hardhat/contracts/Registry.sol
+++ hardhat/contracts/Registry.sol
@@ -9,16 +9,44 @@ import { KYC } from "./KYC.sol";
 /// @notice Registry for authenticated users
 /// @notice Main entrypoint for the feed via a multicall
 contract Registry {
-    mapping(address => IKYC) kycs;
+    mapping(address => IKYC) internal kycs;
+    mapping(bytes => IKYC) internal accountKycs;
+    uint internal kycInstances = 0;
 
-    address st;
+    address internal st;
 
     constructor(address _st) {
         st = _st;
     }
 
-    function createInstance() external {
+    function createInstance() external returns (address) {
         IKYC kyc = new KYC(0, msg.sender, st);
         kycs[msg.sender] = kyc;
+        kycInstances+= 1;
+        return address(kyc);
+    }
+
+    function createInstance(uint sponsorPlatform, bytes memory accountProof) external {
+        IKYC kyc = new KYC(sponsorPlatform, msg.sender, st);
+        kyc.linkAccount(sponsorPlatform, accountProof);
+        // ??
+        accountKycs[accountProof] = kyc;
+        kycInstances+= 1;
+    }
+
+    function getKyc(address user) external view returns (address) {
+        address kycAddress = address(kycs[user]);
+        require(kycAddress != address(0), "No KYC for user");
+        return kycAddress;
+    }
+
+    function getKycForAccount(bytes memory account) external view returns (address) {
+        address kycAddress = address(accountKycs[account]);
+        require(kycAddress != address(0), "No KYC for user");
+        return kycAddress;
+    }
+
+    function getNumberOfInstances() external view returns (uint) {
+        return kycInstances;
     }
 }
